67a3c9d4d11bda8b9152efa698447fee1e9b8658
JayGoal <yj936290344@163.com>
2021-04-19 16:00:23 +0800

    调整匹配方法和日志打印

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import time
+
 import cv2
 import pytesseract
-from PIL import Image
 
 
 def video_show(video):
@@ -11,12 +12,14 @@ def video_show(video):
         ret, frame = video.read()
         # 如果摄像头没开启，则打印信息并退出
         if not ret:
-            print("视频获取失败！")
-            break
+            # print("视频获取失败！")
+            video = cv2.VideoCapture("C:\\Users\\JayGoal\\Desktop\\test.mp4")
+            ret, frame = video.read()
+            # break
         # 如果摄像头正常开启，则输出每帧图像
         cv2.imshow("Video_show", frame)
         # 监控是否按下 Q 键
-        if cv2.waitKey(1) & 0xff == ord("q"):
+        if cv2.waitKey(1) & 0xff == ord("s"):
             select_data = cv2.selectROI("Video_show", frame)
             choose_video = True
         # 如果Q键被按下，则切换显示框选区域
@@ -26,8 +29,11 @@ def video_show(video):
                                 select_data[0]:select_data[0] + select_data[2]]
             cv2.imshow("choose_video", choose_data)
             do_ocr(choose_data)
+        if cv2.waitKey(1) & 0xff == ord('c'):
+            choose_video = False
+            cv2.destroyWindow("choose_video")
         # 如果按下P键，则退出视频
-        if cv2.waitKey(1) & 0xff == ord("p"):
+        if cv2.waitKey(1) & 0xff == ord("q"):
             break
     video.release()
     cv2.destroyAllWindows()
@@ -35,13 +41,16 @@ def video_show(video):
 
 def do_ocr(pic):
     num = pytesseract.image_to_string(pic, lang="eng")
-    num = num.strip()
-    if num == "321321321":
-        print(num)
+    num = num.strip().replace(' ', '')
+    target = '081824303132'
+    if num == target:
+        times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        print("找到特定值！：" + num + "，当前时间：" + times)
 
 
 if __name__ == '__main__':
+    path = "C:\\Users\\JayGoal\\Desktop\\test.mp4"
     # cv2.VideoCapture传入0表示打开摄像头
-    # video_show(cv2.VideoCapture(0))
-    path = "C:\\Users\\JayGoal\\Desktop\\test.png"
-    do_ocr(Image.open(path))
+    video_show(cv2.VideoCapture(path))
+    # do_ocr(Image.open(path))
+
